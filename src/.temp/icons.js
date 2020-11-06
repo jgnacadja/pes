78e5a934eaa1d8c08f3536a0b16bf437c7e8af80
GNACADJA Jacques <jacques.gnacadja@rintio.com>
2020-11-06 16:39:36 +0100

    .

--- src/.temp/icons.js
+++ src/.temp/icons.js
@@ -5,37 +5,37 @@ export default {
   "touchicons": [
     {
       "width": 76,
-      "src": "/assets/static/src/favicon.png?width=76&key=3b53a4a"
+      "src": "/assets/static/src/favicon.png?width=76&key=d9b4636"
     },
     {
       "width": 152,
-      "src": "/assets/static/src/favicon.png?width=152&key=3b53a4a"
+      "src": "/assets/static/src/favicon.png?width=152&key=d9b4636"
     },
     {
       "width": 120,
-      "src": "/assets/static/src/favicon.png?width=120&key=3b53a4a"
+      "src": "/assets/static/src/favicon.png?width=120&key=d9b4636"
     },
     {
       "width": 167,
-      "src": "/assets/static/src/favicon.png?width=167&key=3b53a4a"
+      "src": "/assets/static/src/favicon.png?width=167&key=d9b4636"
     },
     {
       "width": 180,
-      "src": "/assets/static/src/favicon.png?width=180&key=3b53a4a"
+      "src": "/assets/static/src/favicon.png?width=180&key=d9b4636"
     }
   ],
   "favicons": [
     {
       "width": 16,
-      "src": "/assets/static/src/favicon.png?width=16&key=7c74766"
+      "src": "/assets/static/src/favicon.png?width=16&key=5189a38"
     },
     {
       "width": 32,
-      "src": "/assets/static/src/favicon.png?width=32&key=7c74766"
+      "src": "/assets/static/src/favicon.png?width=32&key=5189a38"
     },
     {
       "width": 96,
-      "src": "/assets/static/src/favicon.png?width=96&key=7c74766"
+      "src": "/assets/static/src/favicon.png?width=96&key=5189a38"
     }
   ]
 }
