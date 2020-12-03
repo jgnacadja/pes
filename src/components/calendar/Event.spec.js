9514218bd133cb12414691ac9806d12cba840c7c
Lionel Okpeicha <lohnsonok@gmail.com>
2020-12-03 13:04:31 +0100

    fix: test sonarcloud test cover

---
+++ src/components/calendar/Event.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from "@vue/test-utils";
+import Event from "~/components/calendar/Event.vue";
+
+test("Event", () => {
+  // render the component
+  const wrapper = shallowMount(Event);
+
+  // update the name to be long enough
+  wrapper.setData({ hover: "true" });
+
+  // assert the error has gone away
+  expect(wrapper.find(".error").exists()).toBe(false);
+});
