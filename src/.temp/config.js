9d63c43c5297c50bac9c05ba4e53dd4b1672b755
Lionel Okpeicha <lohnsonok@gmail.com>
2020-12-05 10:54:24 +0100

    fix: code coverage

---
+++ src/.temp/config.js
@@ -0,0 +1,8 @@
+export default {
+  "trailingSlash": true,
+  "pathPrefix": "",
+  "titleTemplate": "%s - Studely",
+  "siteUrl": "",
+  "version": "0.7.21",
+  "catchLinks": true
+}
--- tests/unit/components/event.spec.js
+++ tests/unit/components/event.spec.js
@@ -1,12 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import Event from "~/components/calendar/Event.vue";
+import Event from "../../../src/components/calendar/Event.vue";
 
 test("Event", () => {
   // render the component
   const wrapper = shallowMount(Event);
-
-  // update the name to be long enough
-  wrapper.setData({ hover: "true" });
 
   // assert the error has gone away
   expect(wrapper.find(".error").exists()).toBe(false);
