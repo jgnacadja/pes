1b612c75efd58d248f008a098dd0b371ef99a97d
GNACADJA Jacques <jacques.gnacadja@rintio.com>
2021-01-13 11:07:30 +0100

    fix : update formulaire

--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -1,61 +1,71 @@
 <template>
   <section class="section section-lightest-grey section-partners">
     <div class="container text-center">
-      <div class="logo-list">
-        <g-image
-          alt="APIEX_LOGO"
-          title="APIEX_LOGO"
-          class="institution_logo"
-          src="~/assets/partners/APIEX_LOGO.png"
-        />
+      <div class="columns multiline">
+        <div
+          class="column is-10-desktop is-12-mobile subtitle has-text-centered decoration"
+        >
+          <p>
+            Impulse est une initiative de la chambre de commerce et d'industrie
+            du BENIN, en partenariat avec :
+          </p>
+        </div>
+        <div class="column is-10-desktop is-12-mobile logo-list">
+          <g-image
+            alt="APIEX_LOGO"
+            title="APIEX_LOGO"
+            class="institution_logo"
+            src="~/assets/partners/APIEX_LOGO.png"
+          />
 
-        <g-image
-          alt="ANPME"
-          title="ANPME"
-          class="institution_logo"
-          src="~/assets/partners/ANPME.png"
-        />
+          <g-image
+            alt="ANPME"
+            title="ANPME"
+            class="institution_logo"
+            src="~/assets/partners/ANPME.png"
+          />
 
-        <g-image
-          alt="CEPEPE-1"
-          title="CEPEPE-1"
-          class="institution_logo"
-          src="~/assets/partners/CEPEPE-1.png"
-        />
+          <g-image
+            alt="CEPEPE-1"
+            title="CEPEPE-1"
+            class="institution_logo"
+            src="~/assets/partners/CEPEPE-1.png"
+          />
 
-        <g-image
-          alt="CNSS"
-          title="CNSS"
-          class="institution_logo"
-          src="~/assets/partners/CNSS.png"
-        />
+          <g-image
+            alt="CNSS"
+            title="CNSS"
+            class="institution_logo"
+            src="~/assets/partners/CNSS.png"
+          />
 
-        <g-image
-          alt="DGI"
-          title="DGI"
-          class="institution_logo"
-          src="~/assets/partners/DGI.png"
-        />
+          <g-image
+            alt="DGI"
+            title="DGI"
+            class="institution_logo"
+            src="~/assets/partners/DGI.png"
+          />
 
-        <g-image
-          alt="Ecobank"
-          title="Ecobank"
-          class="institution_logo"
-          src="~/assets/partners/apbef.png"
-        />
+          <g-image
+            alt="Ecobank"
+            title="Ecobank"
+            class="institution_logo"
+            src="~/assets/partners/apbef.png"
+          />
 
-        <g-image
-          alt="UBA"
-          title="UBA"
-          class="institution_logo"
-          src="~/assets/partners/assa.png"
-        />
-      </div>
-      <div class="subtitle decoration">
-        <p>
-          Impulse vous met en relation avec le conseiller d'entreprise qui peut
-          vous aider.
-        </p>
+          <g-image
+            alt="UBA"
+            title="UBA"
+            class="institution_logo"
+            src="~/assets/partners/assa.png"
+          />
+        </div>
+        <div class="column is-10-desktop is-12-mobile subtitle decoration">
+          <p>
+            Impulse vous met en relation avec le conseiller d'entreprise qui
+            peut vous aider.
+          </p>
+        </div>
       </div>
     </div>
   </section>
